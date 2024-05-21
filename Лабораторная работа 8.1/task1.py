aec4446cdb43fd8516022c4fa5ebd3911b1a8f23
Sti1eX <156771118+Sti1eX@users.noreply.github.com>
2024-05-21 08:35:46 +0300

    Add files via upload

---
+++ Лабораторная работа 8.1/task1.py
@@ -0,0 +1,27 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        friends_string = ''
+        for friend in DATABASE.keys():
+            friends_string += friend + ' '
+        # Удаляем лишний пробел в конце строки перед возвратом
+        return 'Твои друзья: ' + friends_string.strip()
+    else:
+        return '<неизвестный запрос>'
+
+# Не изменяйте следующий код
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
